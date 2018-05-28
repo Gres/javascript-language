2cce6ddd942794f5f6f47279243ee78f48f8ef51
Ethan Wang <ewang@cbtnuggets.com>
2018-05-28 13:33:43 -0700

    promise can resolve the same object multiple times

--- node/promise/chainPromisev3.js
+++ node/promise/chainPromisev3.js
@@ -38,6 +38,57 @@ const barXray = fooXray
 console.log(fooXray === fooCopy);
 console.log('-------------------------------------------------');
 
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(1), 1000);
+});
+
+promise.then(function (result) {
+    console.log(result); // 1
+    return result * 2;
+});
+
+promise.then(function (result) {
+    console.log(result); // 1
+    return result * 2;
+});
+
+promise.then(function (result) {
+    console.log(result); // 1
+    return result * 2;
+});
+
+// the resolve value of one promise can be used / referenced multiple times
+console.log('-------------------------------------------------');
+
+let obj = {};
+
+promise = new Promise(function (resolve, reject) {
+    setTimeout(() => { 
+        return resolve(obj);
+    }, 1000);
+});
+
+promise.then(function (result) {
+    console.log(result === obj); // 1
+    console.log(result); // 1
+    return result.a = true;
+});
+
+promise.then(function (result) {
+    console.log(result === obj); // 1
+    console.log(result); // 1
+    return result.b = true;
+});
+
+promise.then(function (result) {
+    console.log(result === obj); // 1
+    console.log(result); // 1
+    return result.c = true;
+});
+
+// the resolve value of one promise can be used / referenced multiple times
+console.log('-------------------------------------------------');
+
 console.log('1. Every foo.then returns a different promise than foo');
 console.log('2. foo.then call does not change foo, foo stays the same');
 console.log('3. we can call then multiple times on foo, each call execute in concurrent and returns distinct promises');
